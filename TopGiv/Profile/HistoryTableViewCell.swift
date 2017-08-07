501fb08933aef808483da83f4cc7d37c14e0b8d2
CodeGhost <mobiledeveloper112075@gmail.com>
2017-08-07 11:43:52 -0400

    Add files via upload

---
+++ TopGiv/Profile/HistoryTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  HistoryTableViewCell.swift
+//  TopGiv
+//
+//  Created by Michael Feigenson on 6/7/17.
+//  Copyright © 2017 Mark. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    @IBOutlet var lb_Category: UILabel!
+    @IBOutlet var lb_Date: UILabel!
+    @IBOutlet var lb_Amount: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
