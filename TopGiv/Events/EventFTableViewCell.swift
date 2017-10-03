59b7c50557ca1cb6cf44d81207690fa260731379
CodeGhost <mobiledeveloper112075@gmail.com>
2017-10-03 12:37:57 -0400

    Add files via upload

--- TopGiv/Events/EventFTableViewCell.swift
+++ TopGiv/Events/EventFTableViewCell.swift
@@ -9,21 +9,19 @@
 import UIKit
 
 class EventFTableViewCell: UITableViewCell {
+    
     @IBOutlet weak var lb_Title: UILabel!
     @IBOutlet weak var img_Back: UIImageView!
     @IBOutlet weak var lb_Place: UILabel!
-    @IBOutlet weak var lb_Mark: UILabel!
     @IBOutlet weak var lb_Date: UILabel!
-    @IBOutlet weak var img_Thumbnail: UIImageView!
-
-    @IBOutlet weak var bt_Like: UIButton!
-    @IBOutlet weak var bt_Dislike: UIButton!
-    @IBOutlet weak var bt_Heart: UIButton!
-    @IBOutlet weak var bt_Share: UIButton!
+//    @IBOutlet weak var bt_Share: UIButton!
+//    @IBOutlet weak var bt_Donate: UIButton!
+//    @IBOutlet weak var bt_Calendar: UIButton!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
